7c00557a16660ca568cd1eb303dc750f64a24d2d
Martineus Fearon <mcqughfearon61@gmail.com>
2025-02-15 15:48:30 -0600

    Updated Level.java
    +Implemented BFS search algorithm and some debug comments.
    +Added ToDo functions

--- src/Classes/Level.java
+++ src/Classes/Level.java
@@ -5,7 +5,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * This class handles parsing a text file containing our level into a navigatable grid
@@ -18,6 +24,7 @@ public class Level {
     private ArrayList<int[]> Map = new ArrayList<>();
     private HashMap<String, ArrayList<String>> NavMap = new HashMap<>();
 
+    //This function generates the map, essentially parsing our text file into a 2d array
     public void GenerateMap(){
         //Read Lines From required file
         ArrayList<String> lines = new ArrayList<>();
@@ -35,20 +42,27 @@ public class Level {
         Map.add(intArray);
         }
 
+        System.out.println("============Map Debug============");
         //View Our Map
+        System.out.println("============Map Generation============");
         for(int[] arr:Map){
             System.out.println(Arrays.toString(arr));
         }
 
         //Generate NavMap
         GenerateNavMap();
+        System.out.println("============Map Generation END============");
 
         // View The NavMap graph
+        System.out.println("============Navigation Map Generation============");
         for (var entry : NavMap.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
+        System.out.println("============Navigation Map Generation END============");
+        System.out.println("================================");
     }
 
+    //This function generates a nav map, essentially creating a dictionary of initial positions:key, and possible positions:value
     public void GenerateNavMap(){
         int gridSize = Map.size();
         
@@ -81,10 +95,67 @@ public class Level {
         }
     }
 
+    //This is a basic search algorithem for now BSF, until i switch to A*
+    public List<String> SimpleSearch(String start, String end){
+        Queue<List<String>> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+
+        queue.add(Collections.singletonList(start));
+
+        while(!queue.isEmpty()){
+            List<String> path = queue.poll();
+            String current = path.get(path.size()-1);
+
+            //Found a path to the target location
+            if(current.equals(end)){
+                return path;
+            }
+
+            if(visited.contains(current)){continue;}
+
+            visited.add(current);
+
+            //Explore neighbor tiles;
+            List<String> neighbors = NavMap.getOrDefault(current, new ArrayList<String>());
+            for (String neighbor: neighbors){
+                List<String> newPath = new ArrayList<>(path);
+                newPath.add(neighbor);
+                queue.add(newPath);
+            }
+
+        }
+       return new ArrayList<>(); //No Valid Paths Found, Empty List Returned
+    }
+
+    //TODO: This function update|create an object mapping to some position on our grid
+    public boolean SetObjectPosition(Object obj, Integer[] position)
+    {
+        return false;
+    }
+
+    //TODO: Function will return the position of the object, (Given the object exists within the map)
+    public Integer[] GetObjectPosition(Object obj){
+        return null;
+    }
+
+    //TODO: Function will determine if a space is occupied by another npc
+    public boolean isOccupied(int x, int y){
+        return false;
+    }
     public static void main(String[] args){
         //Test Map gen
         Level newLevel = new Level();
         newLevel.GenerateMap();
+        
+        String start = "1,1";
+        String end = "19,19";
+
+        List<String> foundPath = newLevel.SimpleSearch(start, end);
+        if(!foundPath.isEmpty()){
+            System.out.println("Navigation is Possible From "+ foundPath);
+        } else {
+            System.out.println("No path found.");
+        }
 
     }
 
