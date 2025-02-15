4942af16198dcf660b07d6b7b4a7be91d695c66c
Martineus Fearon <mcqughfearon61@gmail.com>
2025-02-15 14:20:38 -0600

    Basic Setup For Entity
    +Entity Class
    +Global Enums
    +Status Class

---
+++ src/Classes/Entity.java
@@ -0,0 +1,30 @@
+package src.Classes;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class Entity {
+    //Entity Varaiables
+    boolean isPlayer;
+    Status EntityStats; 
+    Dictionary<Stat,Float> ModifierTable = new Hashtable<>();
+    float Allignment;
+    
+    // Entity Position
+    private int x;
+    private int y;
+
+    //Entity Functions
+    public int[] GetPosition(){
+        int[] Position = {this.x, this.y};
+        return Position;
+    }
+
+    //Get Entity Team
+    public Team GetTeam(){
+        return (this.Allignment>0) ? Team.Human : Team.Technology;
+    }
+}
+
+
+
