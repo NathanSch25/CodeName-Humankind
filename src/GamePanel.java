ddff1c277574a40de2a15ce3426ee04628d0b493
Nathan Schober <nathan.schober25@gmail.com>
2025-02-16 10:51:40 -0600

    Takeover working?

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -190,98 +190,99 @@ public class GamePanel extends JPanel implements Runnable {
             // Player Action
             // Loop through hand
             for (int i =0; i < player.Hand.size(); i++){
-                if ((player.Hand.get(i).Att).TakeOver()){
+                if ((player.Hand.get(i).Att).TakeOver(player)){
                     player.Active = player.Hand.remove(i);
                     player.Active.Att.Attack(player, player.GetPosition(), player.Range);
                     player.RemoveHand();
+
                 }
             }
-                try {
-                    System.out.println("" + player.Hand);
-                    player.Active = player.Hand.remove(PlayerAction - 1);
-                    player.Active.Att.Attack(player, player.GetPosition(), player.Range);
-                    player.RemoveHand();
-                    for (int x = 0; x < ActButtons.length; x++) {
-                        if (x < player.Hand.size()) {
-                            ActButtons[x].setText(player.Hand.get(x).Att.Name);
-                            ActButtons[x].setVisible(true);
-                        } else {
-                            ActButtons[x].setVisible(false);
-                        }
+            try {
+                System.out.println("" + player.Hand);
+                player.Active = player.Hand.remove(PlayerAction - 1);
+                player.Active.Att.Attack(player, player.GetPosition(), player.Range);
+                player.RemoveHand();
+                for (int x = 0; x < ActButtons.length; x++) {
+                    if (x < player.Hand.size()) {
+                        ActButtons[x].setText(player.Hand.get(x).Att.Name);
+                        ActButtons[x].setVisible(true);
+                    } else {
+                        ActButtons[x].setVisible(false);
                     }
-                } catch (IndexOutOfBoundsException ignored) {
-                    player.ShuffleDeck();
-                    ignored.printStackTrace();
-                    //System.out.println("Nope, fuck you");
                 }
+            } catch (IndexOutOfBoundsException ignored) {
+                player.ShuffleDeck();
+                ignored.printStackTrace();
+                //System.out.println("Nope, fuck you");
             }
+        }
             
-            // Do Ally Move + Action
-            //Geting cordants to move to
-            int j;
-            for (j = 0; j < GamePanel.Units.size(); j++){
-                if (!Units.get(j).Good){
-                    break;
-                }
+        // Do Ally Move + Action
+        //Geting cordants to move to
+        int j;
+        for (j = 0; j < GamePanel.Units.size(); j++){
+            if (!Units.get(j).Good){
+                break;
             }
-            Level level = new Level();
-            for(int i = 0; i < GamePanel.Units.size(); i++){
-                if (Units.get(i).Good){
-                    String starting = intToCords(i);
-                    String enimie = intToCords(j);
-                    String cordsL = "";
-                    for(String x : level.SimpleSearch(starting, enimie)){
-                        cordsL += ',' + x;
-                    }
-                    
-                    //moving to found cordnats
-                    int[] cords = cordsToInt(i, cordsL);
-                    GamePanel.Units.get(i).x = cords[0];
-                    GamePanel.Units.get(i).y = cords[1];
-                    System.out.println("teammate moved to " + cords[0] + ", " + cords[1]);
+        }
+        Level level = new Level();
+        for(int i = 0; i < GamePanel.Units.size(); i++){
+            if (Units.get(i).Good){
+                String starting = intToCords(i);
+                String enimie = intToCords(j);
+                String cordsL = "";
+                for(String x : level.SimpleSearch(starting, enimie)){
+                    cordsL += ',' + x;
                 }
                 
-            } 
+                //moving to found cordnats
+                int[] cords = cordsToInt(i, cordsL);
+                GamePanel.Units.get(i).x = cords[0];
+                GamePanel.Units.get(i).y = cords[1];
+                System.out.println("teammate moved to " + cords[0] + ", " + cords[1]);
+            }
+            
+        } 
     
-            // Do Enemy Move + Action
-            //geting cordanats to move to
-            for (j = 0; j < GamePanel.Units.size(); j++){
-                if (!Units.get(j).Good){
-                    break;
-                }
+        // Do Enemy Move + Action
+        //geting cordanats to move to
+        for (j = 0; j < GamePanel.Units.size(); j++){
+            if (!Units.get(j).Good){
+                break;
             }
-            for(int i = 0; i < GamePanel.Units.size(); i++){
-                if (!(Units.get(i).Good)){
-                    String starting = intToCords(i);
-                    String enimie = intToCords(j);
-                    String cordsL = "";
+        }
+        for(int i = 0; i < GamePanel.Units.size(); i++){
+            if (!(Units.get(i).Good)){
+                String starting = intToCords(i);
+                String enimie = intToCords(j);
+                String cordsL = "";
 
-                    for(String x : level.SimpleSearch(starting, enimie)){
-                        cordsL += x;
-                        System.err.println("Our Search Results: "+x);
-                    }
-                    
-                    if("".equals(cordsL)){continue;}
-                    
-                    //moving to found cordnats
-                    int[] cords = cordsToInt(i, cordsL);
-                    GamePanel.Units.get(i).x = cords[0];
-                    GamePanel.Units.get(i).y = cords[1];
-                    System.out.println("enemy moved to " + cords[0] + ", " + cords[1]);
+                for(String x : level.SimpleSearch(starting, enimie)){
+                    cordsL += x;
+                    System.err.println("Our Search Results: "+x);
                 }
                 
-            } 
-
-
-            for (int x = 0; x < Units.size(); x++) {
-                if (Units.get(x).IfHurt()) {
-                    Units.remove(x);
-                    x--;
-                }
+                if("".equals(cordsL)){continue;}
+                
+                //moving to found cordnats
+                int[] cords = cordsToInt(i, cordsL);
+                GamePanel.Units.get(i).x = cords[0];
+                GamePanel.Units.get(i).y = cords[1];
+                System.out.println("enemy moved to " + cords[0] + ", " + cords[1]);
+            }
             
-            player.IfHurt();
-            PlayerTurn = true;
-            return;
+        } 
+
+
+        for (int x = 0; x < Units.size(); x++) {
+            if (Units.get(x).IfHurt()) {
+                Units.remove(x);
+                x--;
+            }
+        
+        player.IfHurt();
+        PlayerTurn = true;
+        return;
         }
     
         if (KeyH.FinishTurn) {
